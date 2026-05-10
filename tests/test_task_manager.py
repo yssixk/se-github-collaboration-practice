5ab4ec453b3f6ea5b1e03a1cb8b9a753bdd576c0
yssixk <yeremia.sy001@gmail.com>
2026-05-10 10:37:02 +0700

    membenarkan pipeline len(result) == 99 ubah ke == 1

--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -69,5 +69,5 @@ def test_search_task_by_assignee():
     tasks = sample_tasks()
     result = search_task_by_assignee(tasks, "rina")
 
-    assert len(result) == 99
+    assert len(result) == 1
     assert result[0]["assignee"] == "Rina"
